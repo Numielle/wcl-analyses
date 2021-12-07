fc1ac7d0f03913b0da699bdacb6b5d4d424abedc
Numielle <Numielle@users.noreply.github.com>
2021-12-07 23:57:38 +0100

    read access token from environment variable and use click for CLI

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
-from warcraftlogs.analysers import VashjAnalyser
+import os
+
+from warcraftlogs.analysers import VashjAnalyser, KaelthasAnalyser
 from warcraftlogs.api import WarcraftLogsAPI
 
-if __name__ == '__main__':
-    report_id = '79qk2H4jCMZTAfwP'  # last PdH SSC
-    at = None
+import click
+
+
+@click.command()
+@click.argument('report_id')
+def main(report_id):
+    at = os.environ.get('AT', default=None)
     api = WarcraftLogsAPI(at)
     v = VashjAnalyser(api, report_id)
-    v.print_core_dunk_times()
-    print()
-    print()
-    # v.print_tainted_elemental_damage_taken()
-    v.print_tainted_uptime()
-    print()
-    print()
-    v.print_strider_uptime()
-    print()
+    v.full_report()
+
+    kt = KaelthasAnalyser(api, report_id)
+    kt.print_phoenix_egg_uptime()
+
+
+if __name__ == '__main__':
+    main()
